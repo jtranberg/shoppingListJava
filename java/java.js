0d7b576c1f73a2a21d4cf705873aae10542ef752
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-01 21:31:40 -0700

    java only shopping list with key up/down/press functions

---
+++ java/java.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#userName');
+
+input.addEventListener('keydown', function(e) {
+    console.log('KEY DOWN!');
+});
+input.addEventListener('keyup', function(e) {
+    console.log('KEY UP!');
+});
+input.addEventListener('keypress', function(e) {
+    console.log('KEY PRESS!');
+});
+
+const addItemInput = document.querySelector('#addItem');
+const itemsUL = document.querySelector('#items');
+
+addItemInput.addEventListener('keypress', function(e) {
+   if(e.key === 'Enter') {
+      const newItemText = this.value;
+      const newItem = document.createElement('li');
+      newItem.innerText = newItemText;
+      itemsUL.appendChild(newItem);
+      this.value = '';
+   }
+});
