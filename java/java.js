dd87aca8bf012a550f4658c987cfad5da352ae81
jtranberg <35013712+jtranberg@users.noreply.github.com>
2022-09-01 22:40:22 -0700

    added notes

--- java/java.js
+++ java/java.js
@@ -1,6 +1,6 @@
-const input = document.querySelector('#userName');
+const input = document.querySelector('#userName');   // create variable
 
-input.addEventListener('keydown', function(e) {
+input.addEventListener('keydown', function(e) {      //events to listen for key strokes
     console.log('KEY DOWN!');
 });
 input.addEventListener('keyup', function(e) {
@@ -9,10 +9,12 @@ input.addEventListener('keyup', function(e) {
 input.addEventListener('keypress', function(e) {
     console.log('KEY PRESS!');
 });
-
+                                                          // create self populating ordered list 
 const addItemInput = document.querySelector('#addItem');
 const itemsOL = document.querySelector('#items');
-
+                                                          //listen for key press
+                                                           //add item
+                                                           //clear placeholder for next item
 addItemInput.addEventListener('keypress', function(e) {
    if(e.key === 'Enter') {
     if(!this.value) return;
